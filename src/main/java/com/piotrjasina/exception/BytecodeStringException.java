b74d2ce91664b0122e1629afdf43ea6a34573a47
petjot <piotrekjasina@gmail.com>
2018-12-10 02:09:33 +0100

    Fix test data loading script

--- src/main/java/com/piotrjasina/exception/BytecodeStringException.java
+++ src/main/java/com/piotrjasina/exception/BytecodeStringException.java
@@ -1,5 +1,6 @@
 package com.piotrjasina.exception;
 
+
 public class BytecodeStringException extends RuntimeException {
     public BytecodeStringException(String message) {
         super(message);
