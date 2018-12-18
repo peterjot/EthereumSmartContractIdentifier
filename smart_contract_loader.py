1973925eb9e41bb3251df414ed522f424cb3df6d
petjot <piotrekjasina@gmail.com>
2018-12-18 20:00:30 +0100

    Update contract loader script to current etherscan website layout

--- smart_contract_loader.py
+++ smart_contract_loader.py
@@ -7,18 +7,18 @@ def main():
     session = requests.session()
 
     print("run script")
-	
-    for i in range(1,10):
+
+    for i in range(1, 10):
         print("run script")
-	
-	
-    for i in range(1,10):
+
+    for i in range(1, 10):
         print("run script")
         response = session.get("https://etherscan.io/contractsVerified/" + str(i) + "?ps=100")
         tree = html.fromstring(response.text)
         print(len(tree.xpath("/html/body/div[1]/div[4]/div[3]/div/div/div/table/tbody/tr/td/a")))
         for address in tree.xpath("/html/body/div[1]/div[4]/div[3]/div/div/div/table/tbody/tr/td/a"):
-            response = session.get("https://api.etherscan.io/api?module=contract&action=getsourcecode&address="+address.text)
+            response = session.get(
+                "https://api.etherscan.io/api?module=contract&action=getsourcecode&address=" + address.text)
             jsonresponse = simplejson.loads(response.content)
             sourceCode = jsonresponse["result"][0]["SourceCode"]
 
@@ -27,7 +27,6 @@ def main():
                 print("ok")
             else:
                 print("failed")
-
 
 
 if __name__ == "__main__":
